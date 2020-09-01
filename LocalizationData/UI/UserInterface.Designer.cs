29973b52e15c9408e8de273b6088bb23c1772cef
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-08-31 22:12:41 -0500

    creating a new project because of an error in the last program

---
+++ LocalizationData/UI/UserInterface.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace LocalizationData
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(426, 12);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 18;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(362, 400);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 13D;
+            this.map.Load += new System.EventHandler(this.map_Load);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(408, 400);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.map);
+            this.Name = "UserInterface";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LocalizationData/UI/UserInterface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using GMap.NET.MapProviders;
+using GMap.NET;
+
+namespace LocalizationData
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+            loadDataGridView();
+        }
+
+        private void loadDataGridView()
+        {
+            string dir = "../../data/uscities.csv";
+            //string dir = "C:/Users/samue/Desktop/a.csv";
+            if (File.Exists(dir))
+            {
+                
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(dir);
+                string[] columns = lines[0].Split(',');
+
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    string[] aux = lines[0].Split(',');
+                    this.dataGridView1.Columns.Add("column" + i, aux[i]);
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    //going per line
+
+                    //split the line by ","
+                    string[] aux = lines[i].Split(',');
+                    string[] rowData = new string[aux.Length];
+
+
+                    for (int j = 0; j < aux.Length; j++)
+                    {
+                        this.dataGridView1.Rows.Add(aux);
+                    }
+
+                }
+            }
+        }
+
+        private void map_Load(object sender, EventArgs e)
+        {
+            map.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            map.Position = new PointLatLng(0, -50);
+        }
+    }
+}

668b6d4c876b3d05ac27e8c816ab9124de168a62
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-09-02 21:49:25 -0500

    minor bugs fixed

--- LocalizationData/UI/UserInterface.cs
+++ LocalizationData/UI/UserInterface.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.IO;
 using GMap.NET.MapProviders;
 using GMap.NET;
+using System.Data;
 
 namespace LocalizationData
 {
@@ -14,6 +15,7 @@ namespace LocalizationData
             loadDataGridView();
         }
 
+        DataTable dt = new DataTable();
         private void loadDataGridView()
         {
             string dir = "../../data/data.csv";
@@ -25,11 +27,15 @@ namespace LocalizationData
                 string[] lines = File.ReadAllLines(dir);
                 string[] columns = lines[0].Split(',');
 
+                
                 for (int i = 0; i < columns.Length; i++)
                 {
                     string[] aux = lines[0].Split(',');
-                    this.dataGridView1.Columns.Add("column" + i, aux[i]);
+                    this.dt.Columns.Add(aux[i]);
+                    
                 }
+                //dt.Columns.Add("djhk");
+                
 
                 for (int i = 1; i < lines.Length; i++)
                 {
@@ -37,23 +43,19 @@ namespace LocalizationData
 
                     //split the line by ","
                     string[] aux = lines[i].Split(',');
-                    string[] rowData = new string[aux.Length];
-
-
-                    for (int j = 0; j < aux.Length; j++)
-                    {
-                        this.dataGridView1.Rows.Add(aux);
-                    }
+                    
+                    dt.Rows.Add(aux);
 
                 }
             }
+            dataGridView1.DataSource = dt;
         }
 
         private void map_Load(object sender, EventArgs e)
         {
             map.MapProvider = GoogleMapProvider.Instance;
             GMaps.Instance.Mode = AccessMode.ServerOnly;
-            map.Position = new PointLatLng(39.47649, -6.37224);
+            map.Position = new PointLatLng(4.60971, -74.08175);
         }
     }
 }
